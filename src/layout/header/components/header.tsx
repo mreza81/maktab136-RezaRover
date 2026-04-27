59663cd1b96ffa529d4f10a8fc0e2616080ea37f
mdghdev81 <mdgh.dev81@gmail.com>
2026-04-27 21:36:02 +0430

    fix:add any images

--- src/layout/header/components/header.tsx
+++ src/layout/header/components/header.tsx
@@ -1,19 +1,37 @@
 function Header() {
 	return (
 		<div>
-			<div>
-				<div className="top mx-auto">
-					<div className="pt-4 bg-black">
+			<div className="bg-black h-70 w-full">
+				<div className="top ">
+					<div className="top-right pt-4">
 						<p className="text-white   font-vazir font-bold hidden lg:block xl:block">
 							رضاروور در شبکه های اجتماعی
 						</p>
 						<img
 							src="../../assets/images/77756d51-bea4-4509-9ae1-8c8d28f62509.png"
 							alt="رضاروور"
-							className="Block text-white"
+							className="Block text-white w-20 h-20"
 						/>
 					</div>
-					<div></div>
+					<div className="top-left">
+						<div>
+							<img
+								src="../../assets/images/instagram.png"
+								alt="instagram"
+								className="w-10 h-10"
+							/>
+							<img
+								src="../../assets/images/twitter-circle.png"
+								alt="twitter"
+								className="w-10 h-10"
+							/>
+							<img
+								src="../../assets/images/whats-app.png"
+								alt="whatsapp"
+								className="w-10 h-10"
+							/>
+						</div>
+					</div>
 				</div>
 				<div className="middle"></div>
 				<div className="bottom"></div>
