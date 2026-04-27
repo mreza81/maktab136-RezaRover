6405f8ca5fb83e03a6cc2e4a30292d7adface209
mdghdev81 <mdgh.dev81@gmail.com>
2026-04-27 20:50:10 +0430

    fix:update header

--- src/layout/header/components/header.tsx
+++ src/layout/header/components/header.tsx
@@ -2,8 +2,17 @@ function Header() {
 	return (
 		<div>
 			<div>
-				<div className="top">
-					<div>رضاروور در شبکه های اجتماعی</div>
+				<div className="top mx-auto">
+					<div className="pt-4 bg-black">
+						<p className="text-white   font-vazir font-bold hidden lg:block xl:block">
+							رضاروور در شبکه های اجتماعی
+						</p>
+						<img
+							src="../../assets/images/77756d51-bea4-4509-9ae1-8c8d28f62509.png"
+							alt="رضاروور"
+							className="Block text-white"
+						/>
+					</div>
 					<div></div>
 				</div>
 				<div className="middle"></div>
