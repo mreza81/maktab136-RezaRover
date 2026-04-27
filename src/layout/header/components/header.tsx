c933e49dbf152cbe66aacfff7095247b6b877087
mdghdev81 <mdgh.dev81@gmail.com>
2026-04-27 16:52:44 +0430

    feat:create header component

---
+++ src/layout/header/components/header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+	return (
+		<div>
+			<div>
+				<div className="top">
+					<div>رضاروور در شبکه های اجتماعی</div>
+					<div></div>
+				</div>
+				<div className="middle"></div>
+				<div className="bottom"></div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
