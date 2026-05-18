a4a02ed52c8d72bf9e48c2ca76d8ba94f4ab020f
mdghdev81 <mdgh.dev81@gmail.com>
2026-05-19 02:21:39 +0430

    fix:update project

--- src/components/admin/dashboard/get products/components/EditProductModal.tsx
+++ src/components/admin/dashboard/get products/components/EditProductModal.tsx
@@ -314,7 +314,7 @@ export default function EditProductModal({
 							</button>
 
 							<button
-								className="px-4 py-2 bg-blue-600 rounded-md "
+								className="px-4 py-2 bg-blue-600 rounded-md disabled:opacity-20"
 								type="submit"
 								disabled={!isAnyEnabled}
 							>
